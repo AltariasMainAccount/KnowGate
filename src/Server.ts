de5a4370993b2e7166f9a4f1b2bc62876c02144b
Philipp Nerlich <p.nerlich@aloberlin.de>
2021-09-27 13:41:44 +0200

    Rendering Engine has been installed successfully

--- src/Server.ts
+++ src/Server.ts
@@ -1,18 +1,35 @@
 // Import CJS modules.
 
 import { Sequelize, Model, DataTypes } from 'sequelize';
+import express from 'express';
+import * as path from 'path';
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     storage: '../../ext/knowgate.sqlite'
 });
 
+export { sequelize };
+
 (async () => {
     try {
         await sequelize.authenticate();
+        console.log("Success!");
     } catch (error) {
         console.error('Unable to connect to the database:', error);
     }
 })();
 
-export { sequelize };
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "../views/"));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+    console.log("Rendered index page.");
+});
+
+app.listen(5000);
--- src/Views/View.ts
+++
@@ -1,3 +0,0 @@
-// Import the CommonJS Modules as EJS modules
-
-import * as express from 'express';
