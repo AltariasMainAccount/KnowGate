bb0974b19294c66425bb4ad1dbf23c2349224851
Altarium <der_lol@web.de>
2021-10-26 13:57:00 +0200

    Update gitignore file

--- out/Models/ModelLoader.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PostComment = exports.Post = exports.Profile = void 0;
-var Profile_1 = require("./Profile");
-Object.defineProperty(exports, "Profile", { enumerable: true, get: function () { return Profile_1.Profile; } });
-var Post_1 = require("./Post");
-Object.defineProperty(exports, "Post", { enumerable: true, get: function () { return Post_1.Post; } });
-var PostComment_1 = require("./PostComment");
-Object.defineProperty(exports, "PostComment", { enumerable: true, get: function () { return PostComment_1.PostComment; } });
